d2735e0b2c2bf5d925fac7ffdb9de7a2385497cc
Alex Wasserman <portbox@gmail.com>
2026-06-26 23:17:45 -0400

    Revert logo change

--- app/Notifications/TemperatureAlertNotification.php
+++ app/Notifications/TemperatureAlertNotification.php
@@ -26,7 +26,7 @@ class TemperatureAlertNotification extends Notification
     {
         return (new WebPushMessage)
             ->title($this->title)
-            ->icon(url('/icons/icon-192.png'))
+            ->icon(url('/favicon.svg'))
             ->body($this->body)
             ->tag('temperature-alert')
             ->renotify()
