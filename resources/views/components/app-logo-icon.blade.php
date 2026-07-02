449331b02279d284f108bd43797e9c32f06cd336
Alex Wasserman <portbox@gmail.com>
2026-07-01 21:29:50 -0400

    Update light mode logo

--- resources/views/components/app-logo-icon.blade.php
+++ resources/views/components/app-logo-icon.blade.php
@@ -1,13 +1,10 @@
-@php
-    $flamePaths = trim(preg_replace('/<\/?svg[^>]*>/', '', file_get_contents(resource_path('svg/flame-mark.svg'))));
-@endphp
-
+<img
+    src="/app-icon-light.png"
+    alt=""
+    {{ $attributes->class(['dark:hidden rounded-md']) }}
+/>
 <img
     src="/pwa-icon-512.png"
     alt=""
     {{ $attributes->class(['hidden dark:block rounded-md']) }}
 />
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1254 1254" {{ $attributes->class(['dark:hidden']) }}>
-    <rect class="fill-white" width="1254" height="1254" rx="274" ry="274"/>
-    {!! $flamePaths !!}
-</svg>
