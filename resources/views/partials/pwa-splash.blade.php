f10c683106099c925443149aa978155b03f0b0c4
Alex Wasserman <portbox@gmail.com>
2026-07-02 22:52:08 -0400

    Turn off apple-mobile-web-app-capable

--- resources/views/partials/pwa-splash.blade.php
+++ resources/views/partials/pwa-splash.blade.php
@@ -16,7 +16,7 @@
     ];
 @endphp
 
-<meta name="apple-mobile-web-app-capable" content="no" />
+{{--<meta name="apple-mobile-web-app-capable" content="yes" />--}}
 <meta name="apple-mobile-web-app-title" content="{{ config('app.name') }}" />
 <meta name="apple-mobile-web-app-status-bar-style" content="{{ config('pwa.status_bar_style') }}" />
 
